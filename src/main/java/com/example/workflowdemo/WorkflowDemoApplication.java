273381d6a21e8bb98e2ebc35bf11807520cc80a1
周风 <FENG17.ZHOU@tcl.com>
2023-02-28 17:25:14 +0800

    初始化

---
+++ src/main/java/com/example/workflowdemo/WorkflowDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.workflowdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/workflowdemo/WorkflowDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workflowdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
